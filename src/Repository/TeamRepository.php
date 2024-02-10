8204d9af49b98b0ff06fc9d3cf69574ee267f615
Luiz Kim <luiz.kim@controleonline.com>
2024-02-10 17:37:08 -0300

    Update

---
+++ src/Repository/TeamRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ControleOnline\Repository;
+
+use ControleOnline\Entity\Team;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class TeamRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Team::class);
+    }
+}
